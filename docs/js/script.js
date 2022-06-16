13812bb58115ea58524a14abfd6ad1dfc0c85672
Christian Widlund <christian.widlund@gmail.com>
2022-06-16 12:33:05 +0200

    functions ...

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,5 +1,4 @@
-const urlParam = function (name) {
-    "use strict";
+const urlParam = name => {
     let results = new RegExp('[?&]' + name + '=([^&#]*)').exec(window.location.href);
     if (results === null) {
         return null;
@@ -8,45 +7,37 @@ const urlParam = function (name) {
     }
 };
 
-let today = new Date();
-let dd = today.getDate();
-let mm = today.getMonth() + 1;
-let yyyy = today.getFullYear();
+const getLunchTime = () => {
+    let today = new Date();
+    let lunchTime = '12:00';
+    if (urlParam('tid') !== null) {
+        lunchTime = urlParam('tid');
+    }
+    today.setHours(...lunchTime.split(':'));
+    return today.getTime();
+};
 
-if (dd < 10) {
-    dd = '0' + dd;
-}
+const renderTimeLeft = (timeLeft, hours, minutes, seconds) => {
+    if (timeLeft > 0) {
+        document.getElementById("tid").innerHTML = 'Om <br/> ' + hours + "h " + minutes + "m " + seconds + "s ";
+    }
 
-if (mm < 10) {
-    mm = '0' + mm;
-}
-
-let timeofday = '12:00:00';
-
-if (urlParam('tid') !== null) {
-    timeofday = urlParam('tid');
-}
-
-let arr = timeofday.split(':');
-arr[0] -= 2;
-timeofday = arr[0] + ':' + arr[1] + ':' + arr[2];
-
-today = yyyy + '-' + mm + '-' + dd + 'T' + timeofday;
-
-let countDownDate = new Date(today).getTime();
-
-const x = setInterval(function () {
-    "use strict";
-    let now = new Date().getTime();
-    let distance = countDownDate - now;
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("tid").textContent = 'Om <br/>' + hours + "h " + minutes + "m " + seconds + "s ";
-
-    if (distance < 0) {
-        clearInterval(x);
+    if (timeLeft < 0) {
+        clearInterval(arDetLunch);
         document.getElementById("tid").textContent = "Ja! Det är lunch!";
     }
+};
+
+const getTimeLeft = () => {
+    let now = new Date().getTime();
+    let timeLeft = getLunchTime() - now;
+    let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    return {timeLeft, hours, minutes, seconds};
+};
+
+const arDetLunch = setInterval(() => {
+    let {timeLeft, hours, minutes, seconds} = getTimeLeft();
+    renderTimeLeft(timeLeft, hours, minutes, seconds);
 }, 1000);
