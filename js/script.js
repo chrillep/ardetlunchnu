60d498f26838a212c91eb9c6a3b949c67bd4734a
Christian Widlund <christian.widlund@gmail.com>
2022-06-19 21:27:39 +0200

    parse string

--- js/script.js
+++ js/script.js
@@ -7,30 +7,39 @@ const urlParam = name => {
     }
 };
 
+const getHoursAndMinutesFromString = lunchTime => {
+    let lunchTimeSplit = lunchTime.split(':');
+    let hours = parseInt(lunchTimeSplit[0]);
+    let minutes = parseInt(lunchTimeSplit[1]);
+    return {hours, minutes};
+};
+
 const getLunchTime = () => {
     let today = new Date();
     let lunchTime = '12:00';
     if (urlParam('tid') !== null) {
         lunchTime = urlParam('tid');
     }
-    today.setHours(...lunchTime.split(':'));
+    let {hours, minutes} = getHoursAndMinutesFromString(lunchTime);
+    today.setHours(hours, minutes, 0, 0);
     return today.getTime();
 };
 
 const renderTimeLeft = (timeLeft, hours, minutes, seconds) => {
     if (timeLeft > 0) {
-        document.getElementById("tid").innerHTML = 'Om <br/> ' + hours + "h " + minutes + "m " + seconds + "s ";
+        document.getElementById("lunchtid").innerHTML = 'Om <br/> ' + hours + "h " + minutes + "m " + seconds + "s ";
     }
 
     if (timeLeft < 0) {
         clearInterval(arDetLunch);
-        document.getElementById("tid").textContent = "Ja! Det är lunch!";
+        document.getElementById("lunchtid").textContent = "Ja! Det är lunch!";
     }
 };
 
 const getTimeLeft = () => {
     let now = new Date().getTime();
-    let timeLeft = getLunchTime() - now;
+    let lunchTime = getLunchTime();
+    let timeLeft = lunchTime - now;
     let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
