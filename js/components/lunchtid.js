5ecab203e538c78e5924d7e1dbc85c9d1ddd4cc2
Christian Widlund <christian.widlund@gmail.com>
2023-05-03 17:45:56 +0200

    add jshint config

--- js/components/lunchtid.js
+++ js/components/lunchtid.js
@@ -1,40 +1,38 @@
 class LunchTid extends HTMLInputElement {
-
-    constructor() {
-        super();
-        if (this.urlParam('tid') !== null) {
-            this.value = this.urlParam('tid');
-        }
-        if (this.urlParam('tid') === null) {
-            this.value = '12:00';
-        }
-        const debouncedUserInput = this.debounce(this.userInput);
-        this.addEventListener('input', debouncedUserInput, false);
+  constructor() {
+    super();
+    if (this.urlParam("tid") !== null) {
+      this.value = this.urlParam("tid");
     }
-
-    urlParam(name) {
-        const queryString = window.location.search;
-        return new URLSearchParams(queryString).get(name);
+    if (this.urlParam("tid") === null) {
+      this.value = "12:00";
     }
+    const debouncedUserInput = this.debounce(this.userInput);
+    this.addEventListener("input", debouncedUserInput, false);
+  }
 
-    userInput() {
-        const queryString = window.location.search;
-        let params = new URLSearchParams(queryString);
-        params.set('tid', this.value);
-        window.location.search = params.toString();
-    }
+  urlParam(name) {
+    const queryString = window.location.search;
+    return new URLSearchParams(queryString).get(name);
+  }
 
-    debounce(func, delay = 2000) {
-        let timerId;
-        return (...args) => {
-            clearTimeout(timerId);
-            timerId = setTimeout(() => {
-                func.apply(this, args);
-            }, delay);
-        };
-    }
+  userInput() {
+    const queryString = window.location.search;
+    let params = new URLSearchParams(queryString);
+    params.set("tid", this.value);
+    window.location.search = params.toString();
+  }
+
+  debounce(func, delay = 2000) {
+    let timerId;
+    return (...args) => {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
+  }
 }
 
 // Define the new element
-customElements.define('lunch-tid', LunchTid, {extends: 'input'});
-
+customElements.define("lunch-tid", LunchTid, { extends: "input" });
--- js/script.js
+++ js/script.js
@@ -1,55 +1,63 @@
-const defaultPageTitle = 'Är det lunch?';
+const defaultPageTitle = "Är det lunch?";
 
-const urlParam = name => {
-    const queryString = window.location.search;
-    return new URLSearchParams(queryString).get(name);
+const urlParam = (name) => {
+  const queryString = window.location.search;
+  return new URLSearchParams(queryString).get(name);
 };
 
-const getHoursAndMinutesFromString = lunchTime => {
-    let decodedLunchTime = decodeURIComponent(lunchTime);
-    let lunchTimeSplit = decodedLunchTime.split(':');
-    let hours = parseInt(lunchTimeSplit[0]);
-    let minutes = parseInt(lunchTimeSplit[1]);
-    return {hours, minutes};
+const getHoursAndMinutesFromString = (lunchTime) => {
+  let decodedLunchTime = decodeURIComponent(lunchTime);
+  let lunchTimeSplit = decodedLunchTime.split(":");
+  let hours = parseInt(lunchTimeSplit[0]);
+  let minutes = parseInt(lunchTimeSplit[1]);
+  return { hours, minutes };
 };
 
 const getLunchTime = () => {
-    let today = new Date();
-    let lunchTime = '12:00';
-    if (urlParam('tid') !== null) {
-        lunchTime = urlParam('tid');
-    }
-    document.title = defaultPageTitle + ' Lunchtid ' + lunchTime;
-    let {hours, minutes} = getHoursAndMinutesFromString(lunchTime);
-    today.setHours(hours, minutes, 0, 0);
-    return today.getTime();
+  let today = new Date();
+  let lunchTime = "12:00";
+  if (urlParam("tid") !== null) {
+    lunchTime = urlParam("tid");
+  }
+  document.title = defaultPageTitle + " Lunchtid " + lunchTime;
+  let { hours, minutes } = getHoursAndMinutesFromString(lunchTime);
+  today.setHours(hours, minutes, 0, 0);
+  return today.getTime();
 };
 
 const renderTimeLeft = (timeLeft, hours, minutes, seconds) => {
-    if (timeLeft > 0) {
-        document.getElementById("lunchtid").innerHTML = 'Om <br/> ' + hours + "h " + minutes + "m " + seconds + "s ";
-        document.title = defaultPageTitle + ' Lunch Om ' + hours + "h " + minutes + "m " + seconds + "s ";
-    }
+  if (timeLeft > 0) {
+    document.getElementById("lunchtid").innerHTML =
+      "Om <br/> " + hours + "h " + minutes + "m " + seconds + "s ";
+    document.title =
+      defaultPageTitle +
+      " Lunch Om " +
+      hours +
+      "h " +
+      minutes +
+      "m " +
+      seconds +
+      "s ";
+  }
 
-    if (timeLeft < 0) {
-        clearInterval(arDetLunch);
-        document.getElementById("lunchtid").textContent = "Ja! Det är lunch!";
-        document.title = defaultPageTitle + " Ja! Det är lunch!";
-
-    }
+  if (timeLeft < 0) {
+    clearInterval(arDetLunch);
+    document.getElementById("lunchtid").textContent = "Ja! Det är lunch!";
+    document.title = defaultPageTitle + " Ja! Det är lunch!";
+  }
 };
 
 const getTimeLeft = () => {
-    let now = new Date().getTime();
-    let lunchTime = getLunchTime();
-    let timeLeft = lunchTime - now;
-    let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-    return {timeLeft, hours, minutes, seconds};
+  let now = new Date().getTime();
+  let lunchTime = getLunchTime();
+  let timeLeft = lunchTime - now;
+  let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  return { timeLeft, hours, minutes, seconds };
 };
 
 const arDetLunch = setInterval(() => {
-    let {timeLeft, hours, minutes, seconds} = getTimeLeft();
-    renderTimeLeft(timeLeft, hours, minutes, seconds);
+  let { timeLeft, hours, minutes, seconds } = getTimeLeft();
+  renderTimeLeft(timeLeft, hours, minutes, seconds);
 }, 1000);
